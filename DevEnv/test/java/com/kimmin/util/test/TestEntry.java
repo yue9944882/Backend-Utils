1c59aa4c5ec74ce355b90f9bbe85688d04f33e47
yue9944882 <291271447@qq.com>
2016-02-15 09:53:28 +0800

    23333

--- DevEnv/test/java/com/kimmin/util/test/TestEntry.java
+++ DevEnv/test/java/com/kimmin/util/test/TestEntry.java
@@ -1,79 +1,35 @@
 package com.kimmin.util.test;
 
 
+
+
+import com.google.common.base.Optional;
 import com.kimmin.util.pc.ExchangeConsumer;
 import com.kimmin.util.pc.ExchangeProducer;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Exchanger;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by min.jin on 2016/2/2.
  */
 public class TestEntry {
+
     public static void main(String[] args){
 
-        Exchanger e=new Exchanger();
-        MyProducer mp=new MyProducer(e);
-        MyConsumer mc=new MyConsumer(e);
-        mp.start();
-        mc.start();
+        ExecutorService fixed=Executors.newFixedThreadPool(4);
+        ExecutorService single=Executors.newSingleThreadExecutor();
+        ExecutorService cached=Executors.newCachedThreadPool();
+        ExecutorService sched=Executors.newScheduledThreadPool(4);
+        ExecutorService singlesched=Executors.newSingleThreadScheduledExecutor();
+        
 
     }
 
-}
 
-
-class MyProducer extends ExchangeProducer<Integer>{
-
-    private BlockingQueue<Integer> bq=new ArrayBlockingQueue<Integer>(100);
-
-    public MyProducer(Exchanger<Integer> ec){
-        super(ec);
-    }
-
-    private Random random=new Random(100);
-
-    @Override
-    public void prepare(){
-        //bq.offer(random.nextInt());
-    }
-
-    @Override
-    public void run(){
-        prepare();
-        while(true){
-            super.produce(random.nextInt());
-            prepare();
-        }
-    }
-}
-
-class MyConsumer extends ExchangeConsumer<Integer>{
-
-    public MyConsumer(Exchanger<Integer> ec){
-        super(ec,1, TimeUnit.SECONDS);
-    }
-
-    @Override
-    public void run(){
-        int cnt=0;
-        while(true){
-            Integer i=super.consume();
-            while(i==null){
-                try{
-                    sleep(100);
-                    i=super.consume();
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Get Value :"+cnt);
-            cnt++;
-        }
-    }
 }
